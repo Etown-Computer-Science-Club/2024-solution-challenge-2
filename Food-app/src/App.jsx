e141cbd0fd86d2d01592ccd7f7239782795a3d98
muzzy3k <144183246+muzzy3k@users.noreply.github.com>
2024-02-15 19:53:06 -0500

    added new account creation, logo, landing page

--- Food-app/src/App.jsx
+++ Food-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { db } from "./firebaseconfig";
 import { collection, addDoc, getDocs, query, where, doc, deleteDoc } from "firebase/firestore";
 import './App.css';
@@ -11,10 +11,14 @@ import { Button } from 'antd';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [email, setEmail] = useState(""); // State for email
+  const [password, setPassword] = useState(""); // State for password
   const [foodItem, setFoodItem] = useState("");
   const [foodList, setFoodList] = useState([]);
   const [date, setDate] = useState("");
   const auth = getAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMode, setModalMode] = useState('signIn'); // 'signIn' or 'signUp'
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
@@ -32,6 +36,35 @@ function App() {
     signInWithPopup(auth, provider).catch((error) => {
       console.error("Error signing in: ", error);
     });
+  };
+
+  const handleEmailSignIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        setIsModalOpen(false); // Close the modal on successful sign-in
+        // Set user state here if needed
+      })
+      .catch((error) => {
+        console.error("Error signing in with email: ", error.message);
+        // Optionally handle errors here
+      });
+  };
+  
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        setIsModalOpen(false); // Close the modal on successful sign-up
+        // You might want to automatically sign the user in here as well
+      })
+      .catch((error) => {
+        console.error("Error signing up: ", error.message);
+        // Optionally handle errors here
+      });
   };
 
   const handleSignOut = () => {
@@ -78,53 +111,98 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        {user ? (
-          <>
-            <nav>
-              <button onClick={handleSignOut} className="sign-out-btn">Sign Out</button>
-              <Link to="/recipes" className="recipes-link">Get Recipes</Link>
-            </nav>
-            
-            <form onSubmit={handleSubmit} className="form">
-              <div>
-                <label>
-                  Food Item:
-                  <input type="text" value={foodItem} onChange={(e) => setFoodItem(e.target.value)} />
-                </label>
-              </div>
-              <div>
-                <label>
-                  Date:
-                  <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-                </label>
-              </div>
-              <button type="submit">Add to Database</button>
-            </form>
-
-            <div className="mt-8">
-              <h2 className="text-center">Food List</h2>
-              <ul>
-                {foodList.map((item) => (
-                  <li key={item.id}>
-                    <Button onClick={() => handleDelete(item.id)} type="link" danger icon={<DeleteTwoTone twoToneColor="#ff4d4f" />} />
-                    {item.foodItem} - {item.date}
-                  </li>
-                ))}
-              </ul>
+  <BrowserRouter>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      {user ? (
+        <>
+          <nav>
+            <button onClick={handleSignOut} className="sign-out-btn">Sign Out</button>
+          </nav>
+          
+          <form onSubmit={handleSubmit} className="form">
+            <div>
+              <label>
+                Food Item:
+                <input type="text" value={foodItem} onChange={(e) => setFoodItem(e.target.value)} />
+              </label>
             </div>
-          </>
-        ) : (
-          <button onClick={handleSignIn}>Sign in with Google</button>
-        )}
+            <div>
+              <label>
+                Date:
+                <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+              </label>
+            </div>
+            <button type="submit">Add to Database</button>
+          </form>
+          <div className="mt-8">
+            <h2 className="text-center">Food List</h2>
+            <ul>
+              {foodList.map((item) => (
+                <li key={item.id}>
+                  <Button onClick={() => handleDelete(item.id)} type="link" danger icon={<DeleteTwoTone twoToneColor="#ff4d4f" />} />
+                  {item.foodItem} - {item.date}
+                </li>
+              ))}
+            </ul>
+            <Link to="/recipes" className="recipes-link">Get Recipes</Link>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="text-center mb-4">
+            <img src="/favicon.png" alt="Logo" style={{ width: "50%", height: "auto" }} className="mb-4" />
+            <h1 className="text-2xl font-bold">WasteNot Pantry</h1>
+          </div>
+          <button onClick={() => setIsModalOpen(true)} className="big-sign-in-btn">Sign In</button>
+        </>
+      )}
 
-        <Routes>
-          <Route path="/recipes" element={<RecipesPage foodList={foodList} />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close-button" onClick={() => setIsModalOpen(false)}>&times;</span>
+            {modalMode === 'signIn' && (
+              <>
+                <button onClick={handleSignIn}>Sign in with Google</button>
+                <button onClick={() => setModalMode('emailSignIn')}>Sign in with Email</button>
+                <button onClick={() => setModalMode('signUp')}>Sign Up</button>
+              </>
+            )}
+            {modalMode === 'emailSignIn' && (
+              <form onSubmit={handleEmailSignIn}>
+                <div>
+                  <label>Email:<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} /></label>
+                </div>
+                <div>
+                  <label>Password:<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} /></label>
+                </div>
+                <button type="submit">Sign In with Email</button>
+                <button type="button" onClick={() => setModalMode('signIn')}>Back</button>
+              </form>
+            )}
+            {modalMode === 'signUp' && (
+              <form onSubmit={handleSignUp}>
+                <div>
+                  <label>Email:<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} /></label>
+                </div>
+                <div>
+                  <label>Password:<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} /></label>
+                </div>
+                <button type="submit">Sign Up</button>
+                <button type="button" onClick={() => setModalMode('signIn')}>Back</button>
+              </form>
+            )}
+          </div>
+        </div>
+      )}
+
+      <Routes>
+        <Route path="/recipes" element={<RecipesPage foodList={foodList} />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+);
+
 }
 
 export default App;
